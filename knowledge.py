f48746f4059131b727cfcbae2624a9a485abaa40
Tharuka Wijethunga <tharuka0621@gmail.com>
2024-12-05 13:02:44 +0530

    knowledge file reformatted.

--- knowledge.py
+++ knowledge.py
@@ -2,13 +2,32 @@ from experta import Fact, KnowledgeEngine, Rule
 
 # Predefined symptoms, animal types, ages, and durations for the expert system
 animal_symptoms = [
-    "fever", "lethargy", "coughing", "diarrhea", "skin_lesions",
-    "appetite_loss", "vomiting", "nasal_discharge"
+    "fever",
+    "lethargy",
+    "coughing",
+    "diarrhea",
+    "skin_lesions",
+    "appetite_loss",
+    "vomiting",
+    "nasal_discharge"
 ]
 
-animal_types = ["Dog", "Cat"]
-ages = ["< 5 yrs", "5 - 10 yrs", "10 < yrs"]
-durations = ["less than 5 days", "5 - 10 days", "more than 10 days"]
+animal_types = [
+    "Dog",
+    "Cat"
+]
+
+ages = [
+    "< 5 yrs",
+    "5 - 10 yrs",
+    "10 < yrs"
+]
+
+durations = [
+    "less than 5 days",
+    "5 - 10 days",
+    "more than 10 days"
+]
 
 # Utility function to validate user input
 def validate_input(user_input, valid_options):
